f7cdcf30c276460778986c267a87d6327a8f46bc
yonmoyonmo <yonmosound@gmail.com>
2021-10-13 04:22:53 +0900

    alarm with voice test

--- lib/screen/alarm_screen.dart
+++ lib/screen/alarm_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:voice_alarm_test_01/main.dart';
+import 'package:voice_alarm_test_01/screen/set_voice_alarm.dart';
+import 'package:voice_alarm_test_01/sound/sound_player.dart';
 
 class AlarmScreen extends StatefulWidget {
   const AlarmScreen({Key? key}) : super(key: key);
@@ -9,8 +10,22 @@ class AlarmScreen extends StatefulWidget {
 }
 
 class _AlarmScreenState extends State<AlarmScreen> {
+  final SoundPlayer voicePlayer = SoundPlayer();
+  @override
+  void initState() {
+    super.initState();
+    voicePlayer.init();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    voicePlayer.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    voicePlayer.togglePlaying();
     return Scaffold(
       body: Center(
         child: Column(
@@ -18,10 +33,16 @@ class _AlarmScreenState extends State<AlarmScreen> {
           children: [
             Text("alarmed"),
             ElevatedButton(
+              onPressed: () async {
+                await voicePlayer.togglePlaying();
+              },
+              child: const Text("stop recorded voice"),
+            ),
+            ElevatedButton(
               onPressed: () {
                 Navigator.push(
                   context,
-                  MaterialPageRoute(builder: (context) => VoiceAlarmHome()),
+                  MaterialPageRoute(builder: (context) => SetVoicAlarm()),
                 );
               },
               child: Text("go back"),
